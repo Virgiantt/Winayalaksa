7a4097257407767efb38b88e8f7480aee4b7bc25
Muhammad Ircham Febriansyah <masirchamganteng@gmail.com>
2025-03-10 14:26:10 +0700

    Edit Menu & announ
    hehe

--- app/views/home/home.php
+++ app/views/home/home.php
@@ -6,11 +6,19 @@
     <ol class="breadcrumb bg-light mt-4">
         <li class="breadcrumb-item"><a href="<?= base_url('user/dashboard') ?>">Home</a></li>
     </ol>
+    <?php if ($announcement['status'] == 1): ?>
+        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            <strong>Pengumuman!</strong> <?= $announcement['text']?>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    <?php endif ?>
     <div class="row" style="margin-bottom:250px">
         <?php
             foreach ($title as $menu) {
         ?>
-        <a href="<?= base_url($menu->link) ?>" class="col-sm-5 text-decoration-none">
+        <a href="<?= base_url($menu->link) ?>" class="col-sm-4 text-decoration-none">
             <div class="bg-warning p-4 my-3 text-white rounded shadow" style="min-height: 160px;">
                 <h3><?= $menu->name ?></h3>
                 <p><?= $menu->desc ?></p>
@@ -19,29 +27,5 @@
         <?php
             }
         ?>
-        <?php if ($announcement['status'] == 1): ?>
-            <div data-toggle="modal" data-target="#detailModal" class="col-sm-5 text-decoration-none">
-                <div class="bg-warning p-4 my-3 text-white rounded shadow" style="min-height: 160px;cursor: pointer;">
-                    <h3>Pengumuman</h3>
-                </div>
-            </div>
-        <?php endif ?>
     </div>
-
-    <div class="modal fade" id="detailModal" tabindex="-1" role="dialog" aria-labelledby="detailModalTitle" aria-hidden="true">
-      <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title text-dark" >Pengumuman</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body text-dark">
-            <h5 style="line-height: 30px;"><?= $announcement['text'] ?></h5>
-          </div>
-        </div>
-      </div>
-    </div>
-
 </div>
